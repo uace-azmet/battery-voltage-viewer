3f44e71e0f1f9c2bfcaa66ea5b9e69cf0a373202
Jeremy Weiss <jlweiss@email.arizona.edu>
2024-08-28 13:05:49 -0700

    added bslib components to app area in html template

--- app/app.R
+++ app/app.R
@@ -25,34 +25,67 @@ library(shiny)
 
 ui <- htmltools::htmlTemplate(
   
-  "azmet-shiny-template.html",
+  filename = "azmet-shiny-template.html",
   
-  sidebarLayout = sidebarLayout(
-    position = "left",
+  #pageFillable = bslib::page_fillable(
+  #  h2("Diamond Plots")
+  #)
+  
+  theme = bslib::bs_theme(
+    version = version_default(),
+    preset = NULL,
+    #...,
+    bg = NULL,
+    fg = NULL,
+    primary = NULL,
+    secondary = NULL,
+    success = NULL,
+    info = NULL,
+    warning = NULL,
+    danger = NULL,
+    base_font = NULL,
+    code_font = NULL,
+    heading_font = NULL,
+    font_scale = NULL,
+    bootswatch = "bootstrap"
+  ),
+  
+  pageSidebar = bslib::page_sidebar(
+    title = NULL,
+    sidebar = "Sidebar",
+    bslib::card(
+      bslib::card_header("Summary")
+    ),
+    bslib::card(
+      bslib::card_header("Table")
+    )
+  )
+  #sidebarLayout = sidebarLayout(
+  #  position = "left",
     
-    #sidebarPanel(
-    #  id = "sidebarPanel",
-    #  width = 4,
+  #  sidebarPanel(
+  #    id = "sidebarPanel",
+  #    width = 4,
       
-    #  verticalLayout(
-    #    selectInput("dataset", label = "Dataset", choices = ls("package:datasets"))
-    #  )
-    #), # sidebarPanel()
-    sidebar = bslib::sidebar(
-      id = "sidebar",
-      open = TRUE,
-      position = "left",
-      width = 250,
-    ), # sidebar()
+  #    verticalLayout(
+  #      selectInput("dataset", label = "Dataset", choices = ls("package:datasets"))
+  #    )
+  #  ), # sidebarPanel()
+    #sidebar = bslib::sidebar(
+    #  id = "sidebar",
+    #  open = TRUE,
+    #  position = "left",
+    #  width = 250,
+    #), # sidebar()
     
-    mainPanel(
-      id = "mainPanel",
-      width = 8,
-      
-      verbatimTextOutput("summary"),
-      tableOutput("table")
-    ) # mainPanel()
-  ) # sidebarLayout()
+    #mainPanel(
+    #  id = "mainPanel",
+    #  width = 8,
+    
+    #  verbatimTextOutput("summary"),
+    #  tableOutput("table")
+    #) # mainPanel()
+  #) # sidebarLayout()
   #sidebar = bslib::sidebar(
   #  id = "sidebar",
   #  open = TRUE,
@@ -84,3 +117,4 @@ server <- function(input, output, session) {
 # Run --------------------
 
 shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, function(input, output) {})
